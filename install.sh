150366e5e1a7ef9017fddc967fb6547167fc54fe
Jefferson Maschio <jeffersonluizdmaschio@gmail.com>
2026-07-23 12:39:40 -0300

    first test of install.sh in a docker container (arch)

--- install.sh
+++ install.sh
@@ -22,26 +22,26 @@ echo -e "\e[34m[Wellcome to Fotfiles Installer]\e[0m"
 
 #ver como pegar se já tem wayland + hyprland baixado
 echo -e "\e[33m[Installing Dependencies...]\e[0m"
-#sudo pacman -S --needed git curl 
+sudo pacman -S --needed git curl 
 
 echo -e "\e[33m[Installing Apps...]\e[0m"
-#sudo pacman -S --needed fastfetch kitty micro rofi swaync waybar wlogout btop nemo
+sudo pacman -S --needed fastfetch kitty micro rofi swaync waybar wlogout btop nemo
 
 echo -e "\e[33m[Installing AUR...]\e[0m"
-#git clone https://aur.archlinux.org/yay.git
-#cd yay
-#mkpkg -si
+git clone https://aur.archlinux.org/yay.git
+cd yay
+mkpkg -si
 	
 echo -e "\e[33m[Installing ZSH...]\e[0m"
-#sudo pacman -S --needed zsh 
+sudo pacman -S --needed zsh 
 		
 echo "List of Plugins:"
 #magenta 
-echo "zsh-autosuggestions     | Auto Suggestions when writtign in terminal"
-echo "zsh-syntax-highlighting | Color if text right or wrong"
-echo "zsh-interactive-cd      | Better cd + tab view with fzf"
-echo "extract                 | Extract any compressed file"
-echo "universalarchive        | Compress any file"
+echo -e "\e[35mzsh-autosuggestions     \e[0m| Auto Suggestions when writtign in terminal"
+echo -e "\e[35mzsh-syntax-highlighting \e[0m| Color if text right or wrong"
+echo -e "\e[35mzsh-interactive-cd      \e[0m| Better cd + tab view with fzf"
+echo -e "\e[35mextract                 \e[0m| Extract any compressed file"
+echo -e "\e[35muniversalarchive        \e[0m| Compress any file"
 
 
 		
@@ -52,24 +52,26 @@ case "${answerP}" in
 	    #install fzf
 	    zsh
 	    sudo pacman -S --needed fzf
-	    #pegar o link de download de cada plugin 
-	    # o extract acho que nao precisa
-	    echo "Downloading zsh-utosuggestions"
+	    
+	    echo "Downloading zsh-utosuggestions..."
 	    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-	    echo "Downloading zsh-syntax-highlighting"
+
+	    echo "Downloading zsh-syntax-highlighting..."
 	    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
 	    echo "Activating interactive-cd"
 	    sleep 0.5
 	    echo "Activing extract"
 	    sleep 0.5
 	    echo "Activing universalarchive"
 	    sleep 0.5
+
 	    echo "Applying Plugins..."
 	    if [ -f "$ZSHRC" ]; then
 	        sed -i 's/plugins=(\(.*\))/plugins=(\1 zsh-autosuggestions\n zsh-syntax-highlighting\n zsh-interactive-cd\n extract\n universalarchive)/' "$ZSHRC"
 	        echo "Plugins Added!!"
 	    else
-	        echo "Error: .zshrc file didn't find."
+	        echo "Error: .zshrc file was not found in the expected location."
 	    fi
 		;;
 	n|no)
