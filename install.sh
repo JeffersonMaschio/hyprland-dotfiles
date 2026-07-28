70ac0f235f4b155b70cb0895af8a2bcc68f9c705
Jefferson Maschio <jeffersonluizdmaschio@gmail.com>
2026-07-28 13:07:53 -0300

    Improve install.sh

--- install.sh
+++ install.sh
@@ -18,125 +18,158 @@
 # Reset Color
 RESET='\033[0m'
 
+RED="\033[0;31m"
 GREEN='\033[0;32m'
-YELLOW='\033[0;33'
-BLUE='\033[0;34'
-PURPLE='\033[0;35'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
  
 ZSHRC="$HOME/.zshrc"
 PACKAGES=(fastfetch kitty micro rofi swaync waybar btop nemo)
 
+run_with_spinner() {
+    local message="$1"
+    shift
 
+    local logfile
+    logfile="$(mktemp)"
 
+    "$@" >"$logfile" 2>&1 &
+    local pid=$!
+
+    local spin_chars='\|/-'
+    local i=0
+
+    printf "%s " "$message"
+    while kill -0 "$pid" 2>/dev/null; do
+        printf "\r%s %s" "$message" "${spin_chars:i++%${#spin_chars}:1}"
+        sleep 0.1
+    done
+
+    wait "$pid"
+    local status=$?
+
+    if [ $status -eq 0 ]; then
+        printf "\r%s ${GREEN}[Done]${RESET}\n" "$message"
+    else
+        printf "\r%s ${RED}[Error]${RESET}\n" "$message"
+        echo -e "${RED}Saída do comando:${RESET}"
+        cat "$logfile"
+    fi
+
+    rm -f "$logfile"
+    return $status
+}
 
 cd $HOME
 clear
-echo -e "\n${BLUE}[Wellcome to Dotfiles Installer]${RESET}"
+printf "\n${BLUE}[Wellcome to Dotfiles Installer]${RESET}\n"
 
 sudo -v || exit 1
 
 #ver como pegar se já tem wayland + hyprland baixado
-printf "\n${YELLOW}[Installing Dependencies...]${RESET}"
 sleep 0.5
-if sudo pacman -S --needed --noconfirm git curl base-devel stow >/dev/null 2>&1; then
-    echo "${GREEN}[Done]${RESET}"
-else
-    echo "[Error installing dependencies]"
+if ! run_with_spinner "[Installing Dependencies]" sudo pacman -S --needed --noconfirm git curl base-devel stow; then
     exit 1
 fi
 
-echo -e "\n${YELLOW}[Installing Apps...]${RESET}"
 sleep 0.5
-if sudo pacman -S --needed --noconfirm "${PACKAGES[@]}" >/dev/null 2>&1; then
-    echo "${GREEN}[Done]${RESET}"
-else
-    echo "[Error installing dependencies]"
+if ! run_with_spinner "[Installing Apps]" sudo pacman -S --needed --noconfirm "${PACKAGES[@]}"; then
     exit 1
 fi
 
 if command -v yay >/dev/null 2>&1; then
-    echo "yay is already installed"
+    printf "${GREEN}[yay is already installed]${RESET}\n"
 else
-    echo -e "\n${YELLOW}[Installing AUR...]${RESET}"
+    printf "\n${YELLOW}[Installing AUR]${RESET}\n"
     sleep 0.5
 
-    git clone https://aur.archlinux.org/yay.git "$HOME/yay"
-    cd "$HOME/yay"
-    makepkg -si --noconfirm
+	if ! run_with_spinner "[Clonning yay]" git clone https://aur.archlinux.org/yay.git "$HOME/yay"; then
+		exit 1
+	fi
+
+    cd "$HOME/yay" || exit 1
+    
+    if ! run_with_spinner "[Compiling]" makepkg -si --noconfirm; then
+    	exit 1
+    fi
+
+    cd "$HOME" || exit 1
 fi
 
-echo -e "\n${YELLOW}[Installing AUR Packages...]${RESET}"
 sleep 0.5
-yay -S --needed --noconfirm wlogout
-	
-echo -e "\n${YELLOW}[Installing ZSH...]${RESET}"
+if ! run_with_spinner "[Installing AUR Packages]" yay -S --needed --noconfirm wlogout; then
+	exit 1
+fi
+
 sleep 0.5
-sudo pacman -S --needed --noconfirm zsh
+if ! run_with_spinner "[Installing ZSH]" sudo pacman -S --needed --noconfirm zsh; then
+	exit 1
+fi
 
 echo ""
 read -r -p "Apply ZSH Default Shell? [Y/n]: " answerZ
 answerZ=${answerZ:-Y}
 case "${answerZ,,}" in
 	[Yy]|[Yy][Ee][Ss])
-		echo "Applying..."	
+		printf "${YELLOW}[Applying...]${RESET}\n"	
 		sleep 0.5
 		sudo usermod -s /bin/zsh "$USER"
-		echo "Done"		
+		printf "${GREEN}[Done]${RESET}\n"		
 		;;
 	[Nn]|[Nn][Oo])
-		echo -e "\n"
+		printf "\n"
 		;;
 	*)
-		echo "Invalid Option"
+		printf "${RED}[Invalid Option]${RESET}\n"
 		;;
 esac
  
-echo -e "\nList of Plugins:"
+printf "\nList of Plugins:\n"
 #magenta 
-echo -e "${PURPLE}zsh-autosuggestions     ${RESET}| Auto Suggestions when writtign in terminal"
-echo -e "${PURPLE}zsh-syntax-highlighting ${RESET}| Color if text right or wrong"
-echo -e "${PURPLE}zsh-interactive-cd      ${RESET}| Better cd + tab view with fzf"
-echo -e "${PURPLE}extract                 ${RESET}| Extract any compressed file"
-echo -e "${PURPLE}universalarchive        ${RESET}| Compress any file"
+printf "${PURPLE}zsh-autosuggestions     ${RESET}| Auto Suggestions when writtign in terminal\n"
+printf "${PURPLE}zsh-syntax-highlighting ${RESET}| Color if text right or wrong\n"
+printf "${PURPLE}zsh-interactive-cd      ${RESET}| Better cd + tab view with fzf\n"
+printf "${PURPLE}extract                 ${RESET}| Extract any compressed file\n"
+printf "${PURPLE}universalarchive        ${RESET}| Compress any file\n"
 
 echo ""
 read -r -p "Install ZSH Plugins? [Y/n]: " answerP
 answerP=${answerP:-Y}
 case "${answerP,,}" in
 	[Yy]|[Yy][Ee][Ss])
-	    sudo pacman -S --needed --noconfirm fzf
+	    run_with_spinner "[Installing fzf]" sudo pacman -S --needed --noconfirm fzf
 	    
-	    echo "Downloading zsh-utosuggestions..."
-	    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+	    run_with_spinner "[Downloading zsh-autosuggestions]" git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
 
-	    echo "Downloading zsh-syntax-highlighting..."
-	    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+	    run_with_spinner "[Downloading zsh-syntax-highlighting]" git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
 
-	    echo "Activating interactive-cd"
+	    printf "${YELLOW}[Activating interactive-cd]${RESET}\n"
 	    sleep 0.5
-	    echo "Activing extract"
+	    printf "${YELLOW}[Activing extract]${RESET}\n"
 	    sleep 0.5
-	    echo "Activing universalarchive"
+	    printf "${YELLOW}[Activing universalarchive]${RESET}\n"
 	    sleep 0.5
 
-	    echo "${YELLOW}Applying Plugins...${RESET}"
+	    printf "${YELLOW}[Applying Plugins...]${RESET}\n"
 	    if [ -f "$ZSHRC" ]; then
 	        sed -i 's/plugins=(\(.*\))/plugins=(\1 zsh-autosuggestions\n zsh-syntax-highlighting\n zsh-interactive-cd\n extract\n universalarchive)/' "$ZSHRC"
-	        echo "${GREEN}[Plugins Added]${RESET}"
+	        printf "${GREEN}[Plugins Added]${RESET}\n"
 	    else
-	        echo "Error: .zshrc file was not found in the expected location."
+	        printf "${RED}[Error: .zshrc file was not found in the expected location]${RESET}\n"
 	    fi	    
 		;;
 	[Nn]|[Nn][Oo])
-		echo -e "\n"
+		printf "\n"
 		;;
 	*)
-		echo "Invalid Option"
+		printf "${RED}[Invalid Option]${RESET}\n"
 		;;
 esac
 
-echo -e "${YELLOW}Creating Symlinks for Config Files...]${RESET}"
-cd ~/hyprland-dotfiles
+printf "${YELLOW}[Creating Symlinks for Config Files...]${RESET}\n"
+mv ~/hyprland-dotfiles ~/dotfiles
+cd ~/dotfiles
 
 stow \
     colors \
